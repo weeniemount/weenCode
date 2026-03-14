24fd9746ba142eccece2420280d181042de3a9c9
weeniemount <59868100+weeniemount@users.noreply.github.com>
2026-03-14 21:01:37 +0100

    Update git.ts

--- extensions/git/src/git.ts
+++ extensions/git/src/git.ts
@@ -10,7 +10,8 @@ import * as cp from 'child_process';
 import { fileURLToPath } from 'url';
 import which from 'which';
 import { EventEmitter } from 'events';
-import { fromBuffer as fileTypeFromBuffer } from 'file-type';
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const fileTypeFromBuffer: (buf: Buffer) => Promise<{ ext: string; mime: string } | undefined> = require('file-type').fromBuffer;
 import { assign, groupBy, IDisposable, toDisposable, dispose, mkdirp, readBytes, detectUnicodeEncoding, Encoding, onceEvent, splitInChunks, Limiter, Versions, isWindows, pathEquals, isMacintosh, isDescendant, relativePathWithNoFallback, Mutable } from './util';
 import { CancellationError, CancellationToken, ConfigurationChangeEvent, LogOutputChannel, Progress, Uri, workspace } from 'vscode';
 import type { Commit as ApiCommit, Ref, Branch, Remote, LogOptions, Change, CommitOptions, RefQuery as ApiRefQuery, InitOptions, DiffChange, Worktree as ApiWorktree } from './api/git';
